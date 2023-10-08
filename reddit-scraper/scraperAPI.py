d11378259265d00e6845bac231d71e6a08a7fe14
jsingh2412 <60803017+jsingh2412@users.noreply.github.com>
2023-10-08 18:58:56 -0500

    Add files via upload

---
+++ reddit-scraper/scraperAPI.py
@@ -0,0 +1,19 @@
+#API BASED SCRAPER
+import praw
+import pandas as pd
+
+reddit_read_only = praw.Reddit(client_id="",         # your client id
+                               client_secret="",      # your client secret
+                               user_agent="")        # your user agent
+ 
+ 
+subreddit = reddit_read_only.subreddit("redditdev")
+ 
+# Display the name of the Subreddit
+print("Display Name:", subreddit.display_name)
+ 
+# Display the title of the Subreddit
+print("Title:", subreddit.title)
+ 
+# Display the description of the Subreddit
+print("Description:", subreddit.description)
