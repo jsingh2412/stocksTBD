3fc29a2d5d99e2ef451288f3442fcae82c796ffc
Chase <chaseburkdoll@gmail.com>
2023-10-20 13:05:24 -0500

    cleaned up useless tailwind & css styles

--- stockstbdui/tailwind.config.js
+++ stockstbdui/tailwind.config.js
@@ -8,15 +8,12 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        satoshi: ["Satoshi", "sans-serif"],
-        inter: ["Inter", "sans-serif"],
         koho: ["KoHo", "sans-serif"],
       },
       fontSize: {
         "5xl": "2rem",
       },
       colors: {
-        "primary-orange": "#FF5722",
         "primary-green": "#587F5C",
       },
     },
