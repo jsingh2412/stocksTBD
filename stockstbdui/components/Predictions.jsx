b1ebdb9409a00fc4815588cad96ea7c9ded19cee
Chase <chaseburkdoll@gmail.com>
2023-10-20 15:44:58 -0500

    Start of StockPrediction

--- stockstbdui/components/Predictions.jsx
+++ stockstbdui/components/Predictions.jsx
@@ -1,9 +1,20 @@
 //Prediction component on dashboard
 
+import StockPrediction from "./Predictions/StockPrediction";
+
 const Predictions = () => {
   return (
-    <div className="bg-primary-gray w-1/2 h-1/2 m-4">
-      <h1>Predictions</h1>
+    <div className="bg-primary-gray w-1/2 h-1/2 m-4 p-4">
+      <h1 className="basic_text_black text-2xl">Predictions</h1>
+      <StockPrediction
+        stock_image=""
+        stock_name="Apple"
+        stock_ticker="aapl"
+        stock_percentage=""
+        stock_suggestion="buy"
+      />
+      <StockPrediction />
+      <StockPrediction />
     </div>
   );
 };
--- stockstbdui/components/Predictions/StockPrediction.jsx
+++ stockstbdui/components/Predictions/StockPrediction.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-const StockPrediction = () => {
-  return <div>StockPrediction</div>;
+const StockPrediction = (props) => {
+  return <div className="w-1/2 bg-green-400">StockPrediction</div>;
 };
 
 export default StockPrediction;
