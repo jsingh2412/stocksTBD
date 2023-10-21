7aa7e3e5472413642449cb3ff9323b88f4349be0
Chase <chaseburkdoll@gmail.com>
2023-10-20 20:43:53 -0500

    Changed positioning on FavoriteStock

--- stockstbdui/components/FavoriteStock.jsx
+++ stockstbdui/components/FavoriteStock.jsx
@@ -1,8 +1,18 @@
-import React from "react";
+import Image from "next/image";
 
-const FavoriteStock = () => {
+const FavoriteStock = (props) => {
   return (
-    <div className="bg-white h-1/6 m-3 trendline flex items-center justify-center">
+    <div className="bg-white h-1/6 m-3 trendline flex items-center justify-center temp">
+      <div className="w-1/5 flex justify-start items-center stocks">
+        <Image
+          src={props.stock_image}
+          alt={props.stock_name}
+          width={60}
+          height={60}
+          className="object-contain"
+        />
+      </div>
+      <h1 className="stocks">{props.stock_name}</h1>
       <svg viewBox="10 20 190 100" className="">
         <path
           d="M10 80 C 30 50, 50 30, 70 80 S 90 50, 110 80 C 130 110, 150 80, 170 70 S 190 60, 200 80"
--- stockstbdui/components/Favorites.jsx
+++ stockstbdui/components/Favorites.jsx
@@ -7,16 +7,32 @@ const Favorites = () => {
     <div className="bg-secondary-green w-1/6 h-3/5 m-4">
       <h1 className="basic_text text-2xl p-3">Favorites</h1>
       <FavoriteStock
-        stock_image=""
+        stock_image="/assets/images/apple-logo.svg"
         stock_ticker=""
         stock_price=""
         stock_vector=""
+        stock_name="apple"
+      />
+      <FavoriteStock
+        stock_image="/assets/images/apple-logo.svg"
+        stock_ticker=""
+        stock_price=""
+        stock_vector=""
+        stock_name="apple"
+      />
+      <FavoriteStock
+        stock_image="/assets/images/apple-logo.svg"
+        stock_ticker=""
+        stock_price=""
+        stock_vector=""
+        stock_name="apple"
       />
       <FavoriteStock
         stock_image=""
         stock_ticker=""
         stock_price=""
         stock_vector=""
+        stock_name="apple"
       />
     </div>
   );
