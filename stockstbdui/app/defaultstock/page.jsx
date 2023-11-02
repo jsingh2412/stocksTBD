bd00d9d662f1e5252860a74f7c6e769a9ea6263b
Chase <chaseburkdoll@gmail.com>
2023-11-02 11:34:13 -0500

    Some very basic styling for Stocks

--- stockstbdui/app/defaultstock/page.jsx
+++ stockstbdui/app/defaultstock/page.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const DefaultStock = () => {
   return (
-    <div>
+    <div className="h-screen bg-secondary-green">
       <Stocks />
     </div>
   );
--- stockstbdui/components/Stocks/Stocks.jsx
+++ stockstbdui/components/Stocks/Stocks.jsx
@@ -1,7 +1,16 @@
 //Information about stocks
+import Image from "next/image";
 
 const Stocks = () => {
-  return <div>Default Stock Page - need to make routing dynamic</div>;
+  return (
+    <div>
+      <p>go back</p>
+      <h1>Ticker</h1>
+      <h2>Name</h2>
+      <Image width={100} height={100} src="/assets/images/Microsoft-logo.svg" />
+      <p>Default Stock Page - need to make routing dynamic</p>
+    </div>
+  );
 };
 
 export default Stocks;
