16b054e69a79b5e5015cc3694af47b7d0f3919e5
Chase <chaseburkdoll@gmail.com>
2023-11-02 11:10:57 -0500

    Filter page routing

--- stockstbdui/components/Header.jsx
+++ stockstbdui/components/Header.jsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 const Header = () => {
   return (
     <div className="w-screen bg-primary-green flex items-center p-1">
-      <Link href=".">
+      <Link href="..">
         <h1 className="basic_text_italic text-5xl pl-5">stocksTBD</h1>
       </Link>
       <Link href="login" className="ml-auto">
--- stockstbdui/components/Predictions/DailyPredictions.jsx
+++ stockstbdui/components/Predictions/DailyPredictions.jsx
@@ -1,7 +1,74 @@
 //Daily predictions page
 
+import Link from "next/link";
+import StockPrediction from "./StockPrediction";
+
 const DailyPredictions = () => {
-  return <div>Daily 5Predictions</div>;
+  return (
+    <div className="m-20">
+      <h1 className="text-3xl basic_text_black">September 26th</h1>
+      <h3 className="text-2xl basic_text_black">Predictions</h3>
+      <Link href="/predictions/filter">
+        <h5>Go to filter page</h5>
+      </Link>
+      <StockPrediction
+        stock_image="/assets/images/apple-logo.svg"
+        stock_name="Apple"
+        stock_ticker="AAPL"
+        stock_percentage="3.5%"
+        stock_suggestion="sell"
+      />
+      <StockPrediction
+        stock_image="/assets/images/Microsoft-logo.svg"
+        stock_name="Microsoft"
+        stock_ticker="MSFT"
+        stock_percentage="2.7%"
+        stock_suggestion="buy"
+      />
+      <StockPrediction
+        stock_image="/assets/images/amazon-logo.svg"
+        stock_name="Amazon"
+        stock_ticker="AMZN"
+        stock_percentage="1.2%"
+        stock_suggestion="sell"
+      />
+      <StockPrediction
+        stock_image="/assets/images/meta-logo.svg"
+        stock_name="META"
+        stock_ticker="META"
+        stock_percentage="7.5%"
+        stock_suggestion="buy"
+      />
+      <StockPrediction
+        stock_image="/assets/images/apple-logo.svg"
+        stock_name="Apple"
+        stock_ticker="AAPL"
+        stock_percentage="3.5%"
+        stock_suggestion="sell"
+      />
+      <StockPrediction
+        stock_image="/assets/images/Microsoft-logo.svg"
+        stock_name="Microsoft"
+        stock_ticker="MSFT"
+        stock_percentage="2.7%"
+        stock_suggestion="buy"
+      />
+      <StockPrediction
+        stock_image="/assets/images/amazon-logo.svg"
+        stock_name="Amazon"
+        stock_ticker="AMZN"
+        stock_percentage="1.2%"
+        stock_suggestion="sell"
+      />
+      <StockPrediction
+        stock_image="/assets/images/meta-logo.svg"
+        stock_name="META"
+        stock_ticker="META"
+        stock_percentage="7.5%"
+        stock_suggestion="buy"
+      />
+    </div>
+  );
 };
 
 export default DailyPredictions;
