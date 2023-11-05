ceab5a61772994f02f4a71990e78ead665579526
Jagroop <jsinghrs.rs@gmail.com>
2023-11-05 16:50:57 -0600

    start of login page, need to fix spacing/colors

--- stockstbdui/app/login/page.jsx
+++ stockstbdui/app/login/page.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+'use client'
+import React, {useState} from 'react';
+import './login.css';
 
 const LogIn = () => {
-  return <div>LogIn</div>;
+  const [action,setAction] = useState('Sign Up');
+
+  return (
+    <div className='container'>
+      <div className="header">
+        <div className="text">{action}</div>
+        <div className="pizzazz"></div>
+      </div>
+      <div className="inputs">
+        <div className="input">
+        <input type="email" placeholder="example@this.com"/>
+        </div>
+        <div className="input">
+        <input type="password" placeholder="********"/>
+        </div>
+      </div>
+      <div className="forgot-password">Lost Password? <span>Click here!</span></div>
+      <div className="submit-container">
+        <div className={action==="Login"?"submit gray":"submit"} onClick={()=>{setAction("Sign Up")}}>Sign Up</div>
+        <div className={action==="Sign Up"?"submit gray":"submit"} onClick={()=>{setAction("Login")}}>Login</div>
+      </div>
+    </div>
+    
+  );
 };
 
 export default LogIn;
