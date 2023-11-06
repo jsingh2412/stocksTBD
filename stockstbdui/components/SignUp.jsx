e0ff96a5d6cb14d538f594be9c26653c3b232a36
Chase <chaseburkdoll@gmail.com>
2023-11-06 11:00:49 -0600

    SignUp responsiveness and drop shadow

--- stockstbdui/components/SignUp.jsx
+++ stockstbdui/components/SignUp.jsx
@@ -1,41 +1,47 @@
 //Sign Up component on dashboard
 
 const SignUp = () => {
-  //Not exactly what i want to do but close!
   const signUpStyle = {
     minHeight: "300px", // Set your desired minimum width in pixels
     minWidth: "150px",
   };
   return (
-    <div className="bg-secondary-green w-1/5 h-2/5 m-4" style={signUpStyle}>
-      <h1 className="basic_text text-3xl p-3">Sign up for</h1>
-      <div className="flex flex-col p-3 h-4/5 items-center justify-center lg:text-2xl sm:text-sm">
-        <label
-          htmlFor="textMessage"
-          className="block md:text-lg sm:text-sm basic_text w-full h-1/8"
-        >
-          Text Messages
-        </label>
-        <input
-          type="text"
-          id="textMessage"
-          placeholder="913-999-9999"
-          className="p-3 basic_text_black text-lg w-full h-1/6"
-        />
-        <label
-          htmlFor="email"
-          className="block text-sm basic_text w-full h-1/8"
-        >
-          Email
-        </label>
-        <input
-          type="email"
-          id="email"
-          placeholder="example@gmail.com"
-          className="p-3 basic_text_black text-lg w-full h-1/6"
-        />
+    <div
+      className="bg-secondary-green w-1/5 h-1/5 m-4 shadow"
+      style={signUpStyle}
+    >
+      <h1 className="basic_text text-2xl p-3">Sign up for</h1>
+      <div className="flex flex-col p-3 h-4/5 items-center justify-center lg:text-2xl text-xl">
+        <div className="flex flex-col">
+          <label
+            htmlFor="textMessage"
+            className="block lg:text-lg text-sm basic_text w-full lg:pb-0 pb-1"
+          >
+            Text Messages
+          </label>
+          <input
+            type="text"
+            id="textMessage"
+            placeholder="913-999-9999"
+            className="p-3 basic_text_black lg:text-lg text-sm w-full"
+          />
+        </div>
+        <div className="flex flex-col">
+          <label
+            htmlFor="email"
+            className="block lg:text-lg text-sm basic_text w-full lg:pb-0 pb-1"
+          >
+            Email
+          </label>
+          <input
+            type="email"
+            id="email"
+            placeholder="example@gmail.com"
+            className="p-3 basic_text_black lg:text-lg text-sm w-full"
+          />
+        </div>
 
-        <button className="bg-primary-gray w-3/4 basic_text_black text-lg h-1/5 mt-auto mb-2 text-center">
+        <button className="bg-primary-gray w-1/2 basic_text_black text-lg h-1/5 mt-auto text-center">
           Join
         </button>
       </div>
