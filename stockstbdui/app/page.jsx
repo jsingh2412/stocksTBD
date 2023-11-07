cfb0155998ed6933a91beee7d5b2c9b88d804fd5
Chase <chaseburkdoll@gmail.com>
2023-11-06 22:46:19 -0600

    Dashboard layout attempt 2

--- stockstbdui/app/page.jsx
+++ stockstbdui/app/page.jsx
@@ -5,32 +5,22 @@ import SignUp from "@components/SignUp";
 import Link from "next/link";
 
 const DashBoard = () => {
-  const newsStyle = {
-    minHeight: "300px",
-    minWidth: "250px",
-  };
-  const predictionStyle = {
-    minHeight: "400px",
-    minWidth: "150px",
-  };
-
   return (
-    <div className="flex justify-center items-center m-4">
-      <div className="grid grid-cols-5 gap-4">
-        <div className="col-span-3 bg-blue-500" style={predictionStyle}>
-          <Predictions />
-        </div>
-        <div className="col-span-1 bg-red-100" style={newsStyle}>
-          <div className="h-full">
-            <Link href="news">
-              <News />
-            </Link>
-          </div>
-        </div>
-        <div className=" col-span-1 bg-yellow-500">
-          <Favorites />
-          <SignUp />
-        </div>
+    <div className="layout bg-blue-500">
+      <div className="predictions-content">
+        <Predictions />
+      </div>
+      <div className="extra-content">Extra Content</div>
+      <div className="news-content">
+        <Link href="news">
+          <News />
+        </Link>
+      </div>
+      <div className="favorites-content">
+        <Favorites />
+      </div>
+      <div className="signup-content">
+        <SignUp />
       </div>
     </div>
   );
--- stockstbdui/components/News.jsx
+++ stockstbdui/components/News.jsx
@@ -6,20 +6,22 @@ const News = () => {
   return (
     <div className="bg-secondary-green">
       <h1 className="basic_text text-2xl p-3">News</h1>
-      <NewsArticle
-        news_image="/assets/images/news-image.webp"
-        news_title="Stock market today: Wall Street closes lower, giving S&P 500 another losing week"
-        news_description="The market posted some gains earlier this week following several healthy indicators on the economy. Wall Street has been watching economic updates ahead of the Federal Reserve’s interest rate policy meeting next week. The central bank is expected to hold interest rates steady after spending much of the last two years pushing rates higher in its bid to tame inflation."
-        news_source="AP NEWS"
-        news_type="finance"
-      />
-      <NewsArticle
-        news_image="/assets/images/news-image.webp"
-        news_title="Stock market today: Wall Street closes lower"
-        news_description="The market posted some gains earlier this week" //following several healthy indicators on the economy. Wall Street has been watching economic updates ahead of the Federal Reserve’s interest rate policy meeting next week. The central bank is expected to hold interest rates steady after spending much of the last two years pushing rates higher in its bid to tame inflation."
-        news_source="AP NEWS"
-        news_type="finance "
-      />
+      <div className="grid grid-cols-1 gap-4">
+        <NewsArticle
+          news_image="/assets/images/news-image.webp"
+          news_title="Stock market today: Wall Street closes lower, giving S&P 500 another losing week"
+          news_description="The market posted some gains earlier this week following several healthy indicators on the economy. Wall Street has been watching economic updates ahead of the Federal Reserve’s interest rate policy meeting next week. The central bank is expected to hold interest rates steady after spending much of the last two years pushing rates higher in its bid to tame inflation."
+          news_source="AP NEWS"
+          news_type="finance"
+        />
+        <NewsArticle
+          news_image="/assets/images/news-image.webp"
+          news_title="Stock market today: Wall Street closes lower"
+          news_description="The market posted some gains earlier this week" //following several healthy indicators on the economy. Wall Street has been watching economic updates ahead of the Federal Reserve’s interest rate policy meeting next week. The central bank is expected to hold interest rates steady after spending much of the last two years pushing rates higher in its bid to tame inflation."
+          news_source="AP NEWS"
+          news_type="finance "
+        />
+      </div>
     </div>
   );
 };
