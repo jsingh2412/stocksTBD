320bca188f04d150aba87d1abe55d5707b2432b5
Chase <chaseburkdoll@gmail.com>
2023-11-07 18:43:20 -0600

    Fix reponsiveness for News component

--- stockstbdui/app/news/page.jsx
+++ stockstbdui/app/news/page.jsx
@@ -2,42 +2,10 @@ import LineChart from "@components/Chart";
 
 import NewsPage from "@components/News/NewsPage";
 
-const data = [
-  {
-    country: "DENMARK",
-    currency: "RDDUSD",
-    type: "RENEWABLE",
-    year: 1975,
-    amount: 0.804,
-  },
-  {
-    country: "DENMARK",
-    currency: "RDDUSD",
-    type: "RENEWABLE",
-    year: 1976,
-    amount: 1.35,
-  },
-  {
-    country: "DENMARK",
-    currency: "RDDUSD",
-    type: "RENEWABLE",
-    year: 1977,
-    amount: 7.928,
-  },
-  {
-    country: "DENMARK",
-    currency: "RDDUSD",
-    type: "RENEWABLE",
-    year: 1978,
-    amount: 15.357,
-  },
-];
-
 const GenericNewsPage = () => {
   return (
     <div className="h-screen bg-secondary-green">
       <NewsPage />
-      <LineChart data={data} height={1000} width={1000} />
     </div>
   );
 };
--- stockstbdui/components/News.jsx
+++ stockstbdui/components/News.jsx
@@ -4,23 +4,49 @@ import NewsArticle from "./NewsArticle";
 
 const News = () => {
   return (
-    <div className="bg-secondary-green">
-      <h1 className="basic_text text-2xl p-3">News</h1>
-      <div className="grid grid-cols-1 gap-4">
-        <NewsArticle
-          news_image="/assets/images/news-image.webp"
-          news_title="Stock market today: Wall Street closes lower, giving S&P 500 another losing week"
-          news_description="The market posted some gains earlier this week following several healthy indicators on the economy. Wall Street has been watching economic updates ahead of the Federal Reserve’s interest rate policy meeting next week. The central bank is expected to hold interest rates steady after spending much of the last two years pushing rates higher in its bid to tame inflation."
-          news_source="AP NEWS"
-          news_type="finance"
-        />
-        <NewsArticle
-          news_image="/assets/images/news-image.webp"
-          news_title="Stock market today: Wall Street closes lower"
-          news_description="The market posted some gains earlier this week" //following several healthy indicators on the economy. Wall Street has been watching economic updates ahead of the Federal Reserve’s interest rate policy meeting next week. The central bank is expected to hold interest rates steady after spending much of the last two years pushing rates higher in its bid to tame inflation."
-          news_source="AP NEWS"
-          news_type="finance "
-        />
+    <div className="grid grid-cols-1 bg-secondary-green news_max">
+      <div>
+        <h1 className="basic_text text-2xl p-3">News</h1>
+      </div>
+      <div className="flex flex-col m-4 max-news">
+        <div className="flex-1">
+          <NewsArticle
+            news_image="/assets/images/news-image.webp"
+            news_title="Stock market today: Wall Street closes lower, giving S&P 500 another losing week"
+            news_description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+            news_source="AP NEWS"
+            news_type="finance"
+          />
+        </div>
+        <div className="flex-1">
+          <NewsArticle
+            news_image="/assets/images/hugestock.png"
+            news_title="The Biggest Stock Market Reversal in History"
+            news_description="There are still 6 weeks or so left in the year but as of this moment the massive swing from huge losses to a gain will likely go down as the biggest stock market reversal in history.
+
+            I looked back at the intra-year losses for the S&P 500 from 1928 through 2020. Here are those intra-year losses paired with how the stock market finished the year:"
+            news_source="A Wealth of Common Sense"
+            news_type="finance "
+          />
+        </div>
+        <div className="flex-1">
+          <NewsArticle
+            news_image="/assets/images/sp.jpg"
+            news_title="Oppenheimer’s Stoltzfus sees stocks bucking drag of high rates"
+            news_description="Wall Street’s biggest bull is sticking to his stock-market optimism, even as concerns mount that corporations will eventually buckle under the pressure of higher interest rates."
+            news_source="Bloomberg"
+            news_type="finance "
+          />
+        </div>
+        <div className="flex-1">
+          <NewsArticle
+            news_image="/assets/images/sp.jpg"
+            news_title="Oppenheimer’s Stoltzfus sees stocks bucking drag of high rates"
+            news_description="Wall Street’s biggest bull is sticking to his stock-market optimism, even as concerns mount that corporations will eventually buckle under the pressure of higher interest rates."
+            news_source="Bloomberg"
+            news_type="finance "
+          />
+        </div>
       </div>
     </div>
   );
