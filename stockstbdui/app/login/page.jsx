f806db2aaabcff98b33dd0ea56b7794c50ff19b8
Jagroop <jsinghrs.rs@gmail.com>
2023-11-13 23:11:32 -0600

    comments for page and some recoloring

--- stockstbdui/app/login/page.jsx
+++ stockstbdui/app/login/page.jsx
@@ -1,15 +1,20 @@
+/*
+// This is the page for the login page, appears when clicking login on the top right of our website.
+// Page will handle both logging in and sign up. Auth still needs to be done.
+// --change colors to better correspond with rest of website
+*/
 'use client'
 import React, {useState} from 'react';
 import './login.css';
 
 const LogIn = () => {
+  //used to change states between login/signup
   const [action,setAction] = useState('Sign Up');
 
   return (
     <div className='container'>
       <div className="header">
         <div className="text">{action}</div>
-        <div className="pizzazz"></div>
       </div>
       <div className="inputs">
         <div className="input">
