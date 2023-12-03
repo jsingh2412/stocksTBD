3f6511e3a61572a29555b1cc906c5ca826bab123
Jagroop <jsinghrs.rs@gmail.com>
2023-12-02 21:43:03 -0600

    home page created

---
+++ stockstbdui/app/home/page.jsx
@@ -0,0 +1,22 @@
+//a new landing page for users to learn about the company
+//before jumping into the dashboard.
+import React from "react";
+import './home.css';
+
+const Home = () => {
+  return (
+    <div>
+        <div className="h-screen bg-secondary-green">
+
+        </div>
+        <div className="h-screen bg-primary-green">
+        
+        </div>
+        <div className="h-screen bg-secondary-green">
+        
+        </div>
+    </div>
+  );
+};
+
+export default Home;
