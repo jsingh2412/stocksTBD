07284929e696d43c6ea508f49bb1760b5e37f4a1
Jagroop <jsinghrs.rs@gmail.com>
2023-12-03 16:01:47 -0600

    update home

--- stockstbdui/app/home/page.jsx
+++ stockstbdui/app/home/page.jsx
@@ -2,7 +2,8 @@
 //before jumping into the dashboard.
 import React from "react";
 import './home.css';
-
+//basic template for the page for now, unsure of what we want here
+//possibly meet the team with cards for each of us?
 const Home = () => {
   return (
     <div>
@@ -11,10 +12,12 @@ const Home = () => {
         <p1>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p1>
         </div>
         <div className="aboutContainer">
-        
+        <h1>About Us</h1>
+        <p1>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p1>
         </div>
         <div className="joinContainer">
-        
+        <h1>Join Now!!!</h1>
+        <p1>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p1>
         </div>
     </div>
   );
