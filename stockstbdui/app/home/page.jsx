9c2d1806039e3562e125b4f4aa11e714b77144d1
Jagroop <jsinghrs.rs@gmail.com>
2023-12-03 13:23:21 -0600

    home page updates

--- stockstbdui/app/home/page.jsx
+++ stockstbdui/app/home/page.jsx
@@ -7,12 +7,13 @@ const Home = () => {
   return (
     <div>
         <div className="header">
-
+        <h1>The future of stock trading</h1>
+        <p1>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p1>
         </div>
         <div className="aboutContainer">
         
         </div>
-        <div className="h-screen bg-secondary-green">
+        <div className="joinContainer">
         
         </div>
     </div>
