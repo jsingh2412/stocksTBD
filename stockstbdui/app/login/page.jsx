34bde16b2c9544e0d8a7a94ba5dc7a39e599241a
Jagroop <jsinghrs.rs@gmail.com>
2024-02-10 23:06:10 -0600

    aesthetics of login page

--- stockstbdui/app/login/page.jsx
+++ stockstbdui/app/login/page.jsx
@@ -9,24 +9,24 @@ import { GoogleSignInButton } from '@components/AuthButtons';
 const LogIn = () => {
   return (
     <div className='w-full flex flex-col items-center'>
-        <div className='flex-col flex m-auto rounded-3xl mt-10 bg-primary-green p-7 max-w-fit'>
+        <div className='flex-col flex m-auto rounded-3xl mt-10 bg-primary-green p-7 max-w-fit shadow-md shadow-slate-400'>
         <div className="flex flex-col items-center gap-1 w-full mt-2">
             <div className="font-koho font-semibold m-auto text-5xl text-white pb-4">Login</div>
         </div>
         <div className="mt-1 flex flex-col justify-center gap-5">
             <div className="flex items-center m-auto w-auto bg-white h-10 rounded-md">
-            <input className="h-12 w-96 bg-transparent border-none outline-none text-lg" type="email" placeholder="example@this.com"/>
+            <input className="h-12 w-96 bg-transparent border-none outline-none text-lg" type="email" placeholder="  example@this.com"/>
             </div>
             <div className="flex items-center m-auto w-auto bg-white h-10 rounded-md">
-            <input className="h-12 w-96 bg-transparent border-none outline-none text-lg" type="password" placeholder="********"/>
+            <input className="h-12 w-96 bg-transparent border-none outline-none text-lg" type="password" placeholder="  ********"/>
             </div>
         </div>
         <div className="flex items-center m-auto mt-2 text-base font-koho mb-2 text-white cursor-pointer"><span>Forgot your password? Click here!</span></div>
         <div className="flex gap-7 m-auto">
-            <div className="flex justify-center items-center w-56 h-16 text-primary-green text-2xl bg-white cursor-pointer rounded-full font-koho font-bold">Login</div>
+            <div className="flex justify-center items-center w-56 h-16 text-primary-green text-2xl bg-white cursor-pointer rounded-3xl font-koho font-bold">Login</div>
         </div>
         </div>
-        <span className='text-2xl font-koho font-semibold text-primary-green text-center mt-2'>----------------or----------------</span>
+        <span className='text-2xl font-koho font-semibold text-primary-green text-center mt-2'>or</span>
         <GoogleSignInButton />
     </div>
     
--- stockstbdui/components/AuthButtons.jsx
+++ stockstbdui/components/AuthButtons.jsx
@@ -14,7 +14,7 @@ export function GoogleSignInButton() {
         onClick={handleClick}
         className="w-auto flex items-center font-semibold justify-normal h-14 px-6 mt-4 
         text-xl transition-colors duration-300 bg-white border-2 border-primary-green text-primary-green 
-        rounded-lg focus:shadow-outline hover:bg-slate-200">
+        rounded-lg focus:shadow-outline hover:bg-slate-200 shadow-md shadow-slate-400">
             <Image src={googleLogo} alt="Google Logo" width={20} height={20} />
             <span className="ml-4">Continue with Google</span>
         </button>
