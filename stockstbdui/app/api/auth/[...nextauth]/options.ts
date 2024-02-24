0ff7a6cb2918c2ba780a5f615b8e0db425b2bb3c
Jagroop <jsinghrs.rs@gmail.com>
2024-02-24 15:51:11 -0600

    changes for login/signup pages user integration start

--- stockstbdui/app/api/auth/[...nextauth]/options.ts
+++ stockstbdui/app/api/auth/[...nextauth]/options.ts
@@ -1,6 +1,7 @@
 import type { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google"
 import CredentialsProvider from "next-auth/providers";
+import { GoogleProfile } from "next-auth/providers/google";
 //CredentialsProvider({
 //            name: "Sign In"
 //        })
@@ -8,10 +9,29 @@ export const options: NextAuthOptions = {
     providers: [
         
         GoogleProvider({
+            profile(profile: GoogleProfile) {
+                console.log(profile.email)
+                return {
+                    ...profile,
+                    role: profile.role ?? "user", 
+                    id:profile.id.toString(),
+                    image: profile.picture
+                }
+            },
             clientId: process.env.GOOGLE_CLIENT_ID as string,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
         })
     ],
+    callbacks: {
+        async jwt({token, user}){
+            if (user) token.role = user.role
+            return token
+        },
+        async session ({session,token}){
+            if (session?.user) session.user.role = token.role
+            return session
+        },
+    },
     pages: {
         signIn: "/login",
         newUser: "/profile"
--- stockstbdui/app/signup/page.jsx
+++ stockstbdui/app/signup/page.jsx
@@ -4,25 +4,56 @@
 */
 'use client'
 import React from 'react';
+import Link from "next/link";
 
 const SignUp = () => {
   return (
-    <div className='flex-col flex m-auto rounded-3xl mt-10 bg-primary-green p-7 max-w-fit'>
-      <div className="flex flex-col items-center gap-1 w-full mt-2">
-        <div className="font-koho font-semibold m-auto text-5xl text-white pb-4">Login</div>
-      </div>
-      <div className="mt-1 flex flex-col justify-center gap-5">
-        <div className="flex items-center m-auto w-auto bg-white h-10 rounded-md">
-        <input className="h-12 w-96 bg-transparent border-none outline-none text-lg" type="email" placeholder="example@this.com"/>
+    <div className='w-full flex flex-col items-center'>
+        <div className='flex-col flex m-auto rounded-3xl mt-10 bg-primary-green p-7 max-w-fit shadow-md shadow-slate-400'>
+          <div className="flex flex-col items-center gap-1 w-full mt-2">
+              <div className="font-koho font-semibold m-auto text-5xl text-white pb-4">Sign Up</div>
+          </div>
+          <div className="mt-1 flex flex-col justify-center gap-0">
+          <div className="flex-col items-center m-auto w-auto">
+                <label htmlFor="password" className="text-sm font-koho text-white mb-1">First Name</label>
+                <div className="bg-white h-auto rounded-md border-primary-green">
+                  <input id="password" className="h-12 w-96 bg-transparent outline-none text-lg p-2" type="name" placeholder="John"/>
+                </div>
+              </div>
+              <div className="flex-col items-center m-auto w-auto">
+                <label htmlFor="password" className="text-sm font-koho text-white mb-1">Last Name</label>
+                <div className="bg-white h-auto rounded-md border-primary-green">
+                  <input id="password" className="h-12 w-96 bg-transparent outline-none text-lg p-2" type="name" placeholder="Doe"/>
+                </div>
+              </div>
+              <div className="flex-col items-center m-auto w-auto">
+                <label htmlFor="password" className="text-sm font-koho text-white mb-0 mt-0">Email</label>
+                <div className="bg-white h-auto rounded-md border-primary-green">
+                  <input id="password" className="h-12 w-96 bg-transparent border-none outline-none text-lg p-2" type="email" placeholder="example@example.com"/>
+                </div>
+              </div>
+              <div className="flex-col items-center m-auto w-auto">
+                <label htmlFor="password" className="text-sm font-koho text-white mb-1">Password</label>
+                <div className="bg-white h-auto rounded-md border-primary-green">
+                  <input id="password" className="h-12 w-96 bg-transparent outline-none text-lg p-2" type="password" placeholder="********"/>
+                </div>
+              </div>
+              <div className="flex-col items-center m-auto w-auto">
+                <label htmlFor="password" className="text-sm font-koho text-white mb-1">Re-enter Password</label>
+                <div className="bg-white h-auto rounded-md border-primary-green">
+                  <input id="password" className="h-12 w-96 bg-transparent outline-none text-lg p-2" type="password" placeholder="********"/>
+                </div>
+              </div>
+          </div>
+          <div className="flex items-center m-auto mt-2 text-base font-koho mb-2 text-white cursor-pointer"><span>Forgot your password? Click here!</span></div>
+          <div className="flex gap-7 m-auto">
+              <div className="flex justify-center items-center w-56 h-16 text-primary-green text-2xl bg-white cursor-pointer rounded-3xl font-koho font-bold">Sign Up</div>
+          </div>
         </div>
-        <div className="flex items-center m-auto w-auto bg-white h-10 rounded-md">
-        <input className="h-12 w-96 bg-transparent border-none outline-none text-lg" type="password" placeholder="********"/>
-        </div>
-      </div>
-      <div className="flex items-center m-auto mt-2 text-base font-koho mb-2 text-white cursor-pointer"><span>Forgot your password? Click here!</span></div>
-      <div className="flex gap-7 m-auto">
-        <div className="flex justify-center items-center w-56 h-16 text-primary-green text-2xl bg-white cursor-pointer rounded-full font-koho font-bold">Login</div>
-      </div>
+        <Link href="login" className="mx-auto pt-2">
+        <span className='text-xl font-koho font-semibold text-primary-green text-center mt-4'>Already have an account?</span>
+        </Link>
+        <button></button>
     </div>
     
   );
