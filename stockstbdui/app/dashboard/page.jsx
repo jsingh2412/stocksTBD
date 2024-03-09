412330173a53a2867d56f6efb360c9e49ee09994
Jagroop <jsinghrs.rs@gmail.com>
2024-03-09 14:58:21 -0600

    dashboard addition and removal of unnecessary content in home page

---
+++ stockstbdui/app/dashboard/page.jsx
@@ -0,0 +1,44 @@
+"use client";
+import Predictions from "@components/Predictions";
+import Favorites from "@components/Favorites";
+import News from "@components/News";
+import SignUp from "@components/SignUp";
+import Link from "next/link";
+import Chart from "@components/Chart";
+import { chartInfo } from "./fakeInfo";
+import ParentSize from "@visx/responsive/lib/components/ParentSize";
+import LandingPage from "@components/LandingPage";
+//Assigns data to chartInfo
+const data = chartInfo;
+
+//Main Dashboard that houses all components for layout.
+const Dashboard = () => {
+    return(
+    <div className="layout m-4">
+      <div className="predictions-content">
+         <Predictions />
+       </div>
+       <div className="extra-content">
+         {/*ParentSize haves the Chart responsive*/}
+         <ParentSize>
+           {({ width, height }) => (
+             <Chart data={data} width={width} height={height} />
+           )}
+         </ParentSize>
+       </div>
+       <div className="news-content">
+         <Link href="news">
+           <News />
+         </Link>
+       </div>
+       <div className="favorites-content">
+         <Favorites height="450px" width="250px" />
+       </div>
+       <div className="signup-content">
+         <SignUp />
+       </div>
+     </div>
+    )
+}
+
+export default Dashboard;
--- stockstbdui/app/page.jsx
+++ stockstbdui/app/page.jsx
@@ -1,45 +1,12 @@
 "use client";
-import Predictions from "@components/Predictions";
-import Favorites from "@components/Favorites";
-import News from "@components/News";
-import SignUp from "@components/SignUp";
-import Link from "next/link";
-import Chart from "@components/Chart";
-import { chartInfo } from "./fakeInfo";
-import ParentSize from "@visx/responsive/lib/components/ParentSize";
 import LandingPage from "@components/LandingPage";
 
-//Assigns data to chartInfo
-const data = chartInfo;
-
-//Main Dashboard that houses all components for layout.
+//A home page to give a small introduction to new users to our website
+//This is a landing page for users that arent authenticated and need to sign in
+//or sign up still.
 const Home = () => {
   return (
     <LandingPage />
-    // <div className="layout m-4">
-    //   <div className="predictions-content">
-    //     <Predictions />
-    //   </div>
-    //   <div className="extra-content">
-    //     {/*ParentSize haves the Chart responsive*/}
-    //     <ParentSize>
-    //       {({ width, height }) => (
-    //         <Chart data={data} width={width} height={height} />
-    //       )}
-    //     </ParentSize>
-    //   </div>
-    //   <div className="news-content">
-    //     <Link href="news">
-    //       <News />
-    //     </Link>
-    //   </div>
-    //   <div className="favorites-content">
-    //     <Favorites height="450px" width="250px" />
-    //   </div>
-    //   <div className="signup-content">
-    //     <SignUp />
-    //   </div>
-    // </div>
   );
 };
 
