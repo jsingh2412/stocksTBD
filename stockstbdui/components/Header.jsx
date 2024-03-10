54f454959af90583dd58c091df8a7362a49b818c
Chase <chaseburkdoll@gmail.com>
2024-03-10 11:36:30 -0500

    Updated Header to move to dashboard

--- stockstbdui/components/Header.jsx
+++ stockstbdui/components/Header.jsx
@@ -1,14 +1,14 @@
 //The header of our webapp that will be present on all other pages.
 import Link from "next/link";
-import HeaderLogIn from '@components/headerLogIn'
+import HeaderLogIn from "@components/headerLogIn";
 //need to make utility classes for text so im not repeating again
 const Header = () => {
   return (
     <div className="w-screen bg-primary-green flex items-center p-1">
-      <Link href="..">
+      <Link href="../dashboard">
         <h1 className="basic_text_italic text-3xl pl-5">stocksTBD</h1>
       </Link>
-      <HeaderLogIn/>
+      <HeaderLogIn />
     </div>
   );
 };
