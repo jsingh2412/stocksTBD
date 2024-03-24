b0b1da009f453d96aa0af74caf6900e18cbeea6c
Jagroop <jsinghrs.rs@gmail.com>
2024-03-23 20:58:29 -0500

    changes to accomadate users in database

---
+++ stockstbdui/app/api/db/db.js
@@ -0,0 +1,26 @@
+import mysql from 'mysql2/promise';
+
+export default async function executeQuery(query, data){
+    try {
+        const db = await mysql.createConnection({
+            host: 'localhost',
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: 'stockstbd',
+        });
+        const [result] = await db.execute(query, data);
+        await db.end();
+        return result;
+    } catch (error){
+        console.log(error);
+        return null;
+    }
+}
+
+export async function checkUser(username, password){
+    try{
+        const response = await executeQuery("SELECT" + username + "FROM users WHERE password=\"" + password + "\"", []);
+    } catch(error){
+        throw new Error("No user found.");
+    }
+}
